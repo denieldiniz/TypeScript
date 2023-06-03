cfb3d8599acb3c7f7a95ea1069775577062e8903
denieldiniz <denieldiniz@gmail.com>
2023-06-03 17:52:08 -0300

    iniciando 8_CLASSES

--- 7_CRIACAO_DE_TIPOS/index.js
+++ 7_CRIACAO_DE_TIPOS/index.js
@@ -10,13 +10,28 @@ function mostrarString(entrada) {
 function descreverProduto(produto) {
     return `O produto se chama ${produto.nome}.`;
 }
-let item1 = { nome: "telefone celular", geracao: 3 };
+let item1 = {
+    nome: "telefone celular",
+    geracao: 3
+};
 // console.log(descreverProduto(item1));
-let item2 = { nome: "telemóvel particular" };
-const primeiroCarro = { nome: "chevete", cor: "grafite", bluetooth: false, estoque: 10 };
-const headphone1 = { nome: "KZ", cor: "preto", bluetooth: 5.0, estoque: "loja 5" };
+let item2 = {
+    nome: "telemóvel particular"
+};
+const primeiroCarro = {
+    nome: "chevete",
+    cor: "grafite",
+    bluetooth: false,
+    estoque: 10
+};
+const headphone1 = {
+    nome: "KZ",
+    cor: "preto",
+    bluetooth: 5.0,
+    estoque: "loja 5"
+};
 // console.log(primeiroCarro);
-// console.log(headphone1);
+//  console.log(headphone1);
 //------------------------------------------------------
 // 4 - Type parameters
 function getKey(obj, key) {
@@ -34,8 +49,29 @@ const primeiraPessoa = {
     idade: 41,
     adulto: true
 };
-console.log(mostrarDadoDaPessoa(primeiraPessoa, "nome"));
-console.log(mostrarDadoDaPessoa(primeiraPessoa, "idade"));
-console.log(mostrarDadoDaPessoa(primeiraPessoa, "adulto"));
+// console.log(mostrarDadoDaPessoa(primeiraPessoa, "nome"));
+// console.log(mostrarDadoDaPessoa(primeiraPessoa, "idade"));
+// console.log(mostrarDadoDaPessoa(primeiraPessoa, "adulto"));
+// console.log(`lalala ${primeiraPessoa.idade}`); //teste
 //------------------------------------------------------
-// 6 - 
+// 6 - Typeof type operator
+const login = "denieldiniz";
+const senha = "234games ";
+const caminhao = {
+    tipo: "caminhão",
+    cor: "azul",
+    km: 12000,
+    empracado: true
+};
+function mostraKm(km) {
+    console.log(`O veículo rodou ${km}`);
+}
+;
+// mostraKm(caminhao['km']);
+// mostraKm(caminhao.km);
+//------------------------------------------------------
+// 8 - Conditional expressions type
+// cria dus interfaces, uma extendendo a outra
+//testa se B extends A
+//------------------------------------------------------
+// 9 - Template literals type
--- 7_CRIACAO_DE_TIPOS/index.ts
+++ 7_CRIACAO_DE_TIPOS/index.ts
@@ -1,10 +1,10 @@
 //------------------------------------------------------
 // 1 - Generics
 
-function mostrarString<T>(entrada: T): string {
+function mostrarString < T > (entrada: T): string {
 
     return `o valor dado é "${entrada}".`
-    
+
 }
 
 // console.log(mostrarString("teste"));
@@ -12,40 +12,57 @@ function mostrarString<T>(entrada: T): string {
 //------------------------------------------------------
 // 2 - Constraint em generic
 
-function descreverProduto<T extends {nome: string}> (produto: T): string {
+function descreverProduto < T extends {
+    nome: string
+} > (produto: T): string {
     return `O produto se chama ${produto.nome}.`;
 }
 
-let item1 = {nome: "telefone celular", geracao: 3};
+let item1 = {
+    nome: "telefone celular",
+    geracao: 3
+};
 // console.log(descreverProduto(item1));
 
-let item2 = {nome: "telemóvel particular"};
+let item2 = {
+    nome: "telemóvel particular"
+};
 // console.log(descreverProduto(item2));
 
 //------------------------------------------------------
 // 3 - Generics com interface
 
-interface Produto<T, U, Q> {
+interface Produto < T, U, Q > {
     nome: string;
     cor: T;
     bluetooth: U;
     estoque: Q;
 }
 
-type carro      = Produto<string, boolean, number>;
-type headPhone  = Produto<string, number, string>;
+type carro = Produto < string, boolean, number > ;
+type headPhone = Produto < string, number, string > ;
 
 
-const primeiroCarro: carro  = {nome: "chevete",cor: "grafite",bluetooth: false,estoque: 10};
-const headphone1: headPhone = {nome: "KZ", cor: "preto", bluetooth: 5.0, estoque: "loja 5"};
+const primeiroCarro: carro = {
+    nome: "chevete",
+    cor: "grafite",
+    bluetooth: false,
+    estoque: 10
+};
+const headphone1: headPhone = {
+    nome: "KZ",
+    cor: "preto",
+    bluetooth: 5.0,
+    estoque: "loja 5"
+};
 
 // console.log(primeiroCarro);
-// console.log(headphone1);
+//  console.log(headphone1);
 
 //------------------------------------------------------
 // 4 - Type parameters
 
-function getKey<T, K extends keyof T>(obj: T, key: K) {
+function getKey < T, K extends keyof T > (obj: T, key: K) {
     return `A chave ${String(key)} está presente no objeto e tem o valor de ${obj[key]}`;
 }
 
@@ -66,7 +83,11 @@ const server = {
 // insanciar o promeiro tipo
 // chamar a função passando o objeto criado e seu atributo/chave
 
-type Pessoa = {nome: string, idade: number, adulto: boolean}
+type Pessoa = {
+    nome: string,
+    idade: number,
+    adulto: boolean
+}
 
 type C = keyof Pessoa;
 
@@ -75,14 +96,64 @@ function mostrarDadoDaPessoa(pessoa: Pessoa, key: C): string {
 }
 
 const primeiraPessoa = {
-    nome: "Deniel", 
+    nome: "Deniel",
     idade: 41,
     adulto: true
 }
 
-console.log(mostrarDadoDaPessoa(primeiraPessoa, "nome"));
-console.log(mostrarDadoDaPessoa(primeiraPessoa, "idade"));
-console.log(mostrarDadoDaPessoa(primeiraPessoa, "adulto"));
+// console.log(mostrarDadoDaPessoa(primeiraPessoa, "nome"));
+// console.log(mostrarDadoDaPessoa(primeiraPessoa, "idade"));
+// console.log(mostrarDadoDaPessoa(primeiraPessoa, "adulto"));
+// console.log(`lalala ${primeiraPessoa.idade}`); //teste
 
 //------------------------------------------------------
-// 6 - 
+// 6 - Typeof type operator
+
+const login: string = "denieldiniz";
+
+type X = typeof login;
+
+const senha: X = "234games ";
+
+// console.log(senha);
+
+//------------------------------------------------------
+// 7 - Indexed access type
+
+// define o tipo literal
+//  define o tipo que referencia o atributo do dipo literal
+// instancia objeto do primeiro tipo
+// cria função que recebe o objeto como parametro e retorna o atributo referenciado
+
+type Veiculo = {
+    tipo: string,
+    cor: string,
+    km: number,
+    empracado: boolean
+};
+
+type Km = Veiculo["km"];
+
+const caminhao: Veiculo = {
+    tipo: "caminhão",
+    cor: "azul",
+    km: 12000,
+    empracado: true
+};
+
+function mostraKm(km: Km): void {
+    console.log(`O veículo rodou ${km}`);
+};
+
+// mostraKm(caminhao['km']);
+// mostraKm(caminhao.km);
+
+//------------------------------------------------------
+// 8 - Conditional expressions type
+
+// cria dus interfaces, uma extendendo a outra
+//testa se B extends A
+
+//------------------------------------------------------
+// 9 - Template literals type
+
