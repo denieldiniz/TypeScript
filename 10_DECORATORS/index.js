ba86cb5c119b68820b72240dbb2ea1c0c17ba0e3
denieldiniz <denieldiniz@gmail.com>
2023-06-15 23:58:13 -0300

    meio decoretors

--- 10_DECORATORS/index.js
+++ 10_DECORATORS/index.js
@@ -1,60 +1,75 @@
 "use strict";
 //------------------------------------------------------
 // 01 - Exemplo decorator
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-// target
-// propertKey
-// descriptor
 function meuDecorator() {
     console.log("Iniciando decorator.");
     return function (target, propertKey, descriptor) {
         console.log("Executando decorator.");
-        console.log(target);
-        console.log(propertKey);
-        console.log(descriptor);
+        // console.log(target);
+        // console.log(propertKey);
+        // console.log(descriptor);
     };
 }
-class minhaClass {
-    // @meuDecorator();
-    testing() {
+class MinhaClasse {
+    // @meuDecorator()  // << rodando decorator ?!?!! Oo
+    testando() {
         console.log("Terminando a execução do método.");
     }
+    ;
 }
-// const meuObjeto = new minhaClass();
-// meuObjeto.testing();
+const meuObjeto = new MinhaClasse();
+// meuObjeto.testando();
 //------------------------------------------------------
 // 02 - Multiplos ddecorators
 function decoratorUm() {
     return function (target, propertKey, descriptor) {
         console.log("executou decorator 1.");
+        // console.log("S2 Cocadinha");
     };
 }
 function decoratorDois() {
     return function (target, propertKey, descriptor) {
         console.log("Executou decorator 2.");
+        // console.log("S2 minha branquinha");
     };
 }
 class VariosDecorators {
-    teste() {
+    // @decoratorUm()
+    // @decoratorDois()
+    finalizar() {
         console.log("Fim da execução.");
     }
 }
-__decorate([
-    decoratorUm
-], VariosDecorators.prototype, "", void 0);
-__decorate([
-    decoratorDois
-], VariosDecorators.prototype, "", void 0);
-const descorators = new VariosDecorators();
+// const descorators = new VariosDecorators();
+// descorators.finalizar();
 //------------------------------------------------------
-// 03 - 
+// 03 - Class decorator
+function classDecorator(constructor) {
+    if (constructor.name === "User") {
+        console.log("Criando usuário.");
+        // console.log(constructor);
+    }
+}
+// @classDecorator  //esse troço roda aparentemete de qqr jeito.
+class User {
+    constructor(name) {
+        this.name = name;
+    }
+}
+// const deniel = new User("Deniel");
+// console.log(deniel);
 //------------------------------------------------------
-// 04 - 
+// 04 - Method decorator
+class Machine {
+    constructor(name) {
+        this.name = name;
+    }
+    showName() {
+        return (`O nome da máquina é ${this.name}.`);
+    }
+}
+const trator = new Machine("\"trator pequeno\"");
+console.log(trator.showName());
 //------------------------------------------------------
 // 05 - 
 //------------------------------------------------------
--- 10_DECORATORS/index.ts
+++ 10_DECORATORS/index.ts
@@ -1,10 +1,6 @@
 //------------------------------------------------------
 // 01 - Exemplo decorator
 
-// target
-// propertKey
-// descriptor
-
 function meuDecorator() {
     console.log("Iniciando decorator.");
 
@@ -14,24 +10,21 @@ function meuDecorator() {
         descriptor: PropertyDescriptor
         ) {
         console.log("Executando decorator.");
-        console.log(target);
-        console.log(propertKey);
-        console.log(descriptor);
+        // console.log(target);
+        // console.log(propertKey);
+        // console.log(descriptor);
     };
 }
 
-class minhaClass {
-    nome!: string;
-
-    // @meuDecorator();
-
-    testing() {
+class MinhaClasse {
+    // @meuDecorator()  // << rodando decorator ?!?!! Oo
+    testando() {
         console.log("Terminando a execução do método.");        
-    }
+    };
 }
 
-// const meuObjeto = new minhaClass();
-// meuObjeto.testing();
+const meuObjeto = new MinhaClasse();
+// meuObjeto.testando();
 
 //------------------------------------------------------
 // 02 - Multiplos ddecorators
@@ -39,47 +32,80 @@ class minhaClass {
 function decoratorUm() {
 
     return function (
-        target: any, 
-        propertKey: string, 
+        target: any,
+        propertKey: string,
         descriptor: PropertyDescriptor
         ) {
-        console.log("executou decorator 1."); 
-    }
+        console.log("executou decorator 1.");
+        // console.log("S2 Cocadinha");
+    };
 }
 
 function decoratorDois() {
 
     return function (
-        target: any, 
-        propertKey: string, 
+        target: any,
+        propertKey: string,
         descriptor: PropertyDescriptor
         ) {
         console.log("Executou decorator 2.");
-    }
+        // console.log("S2 minha branquinha");
+    };
 }
 
 class VariosDecorators {
-    @decoratorUm;
-    @decoratorDois;
-    teste() {
+    // @decoratorUm()
+    // @decoratorDois()
+    finalizar() {
         console.log("Fim da execução.");
-        
     }
 }
 
-const descorators = new VariosDecorators();
+// const descorators = new VariosDecorators();
+// descorators.finalizar();
 
 
 
 //------------------------------------------------------
-// 03 - 
+// 03 - Class decorator
 
+function classDecorator(constructor: Function) {
+    if (constructor.name === "User") {
+        console.log("Criando usuário.");
+        // console.log(constructor);
+    }
+}
 
+// @classDecorator  //esse troço roda aparentemete de qqr jeito.
+
+class User {
+    name;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+// const deniel = new User("Deniel");
+// console.log(deniel);
 
 //------------------------------------------------------
-// 04 - 
+// 04 - Method decorator
 
+class Machine {
+    name;
 
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    showName() {
+        return(`O nome da máquina é ${this.name}.`);
+    }
+}
+
+const trator = new Machine("\"trator pequeno\"");
+console.log(trator.showName());
 
 //------------------------------------------------------
 // 05 - 
@@ -108,5 +134,15 @@ const descorators = new VariosDecorators();
 
 //------------------------------------------------------
 // 10 - 
+
+
+
+
+
+
+
+
+
+
 
 
