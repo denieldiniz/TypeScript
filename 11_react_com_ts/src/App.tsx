ff4d1975f98590779a084dc498938ac67aa81bd8
denieldiniz <denieldiniz@gmail.com>
2023-06-24 18:59:00 -0300

    08 - Type

--- 11_react_com_ts/src/App.tsx
+++ 11_react_com_ts/src/App.tsx
@@ -5,6 +5,7 @@ import { FirstComponent } from "./components/FirstComponent";
 import SecondComponent from "./components/SecondCOmponent";
 import Desestruturado from "./components/Desestruturado";
 import State from "./components/State";
+type textOrNull = string | null; // 08 - Type
 
 function App() {
   //------------------------------------------------------
@@ -19,10 +20,17 @@ function App() {
     return `Olá ${nome}!`;
   };
 
+  //------------------------------------------------------
+  // 08 - Type
+  const umTexto: textOrNull = "receba um texto";
+  let outroTexto: textOrNull = null;
+  outroTexto = "opa";
+  // umTexto = "Texto recebido";  // da erro por ser uma constante
+
+  //----------------------------------------------------------------//
   return (
     <div className="App">
       <h1>Typescript com React</h1>
-
       {/* 01 - Variáveis -------------------- */}
       <h3>Nome: {nome} (aula 1)</h3>
       <h4>idade: {idade}</h4>
@@ -35,29 +43,24 @@ function App() {
       </h4>
       <br />
       <br />
-
       {/* 02 - Funções ---------------------- */}
       <h3>{saudacao(nome)} (aula 2) </h3>
       <br />
       <br />
-
       {/* 04 - Importação de componentes ---- */}
       <FirstComponent />
       <br />
       <br />
-
       {/* 05 - Destructurig nas Props ------- */}
       <SecondComponent nome="SecondComponent" />
       <br />
       <br />
-
       <Desestruturado
         titulo="Senado aprova Cristiano Zanin como novo ministro do Supremo Tribunal Federal"
         conteudo="O advogado Cristiano Zanin é o novo ministro do Supremo Tribunal Federal. Em data ainda não marcada, ele vai assumir a vaga de Ricardo Lewandowski, que se aposentou em abril. Nesta quarta-feira (21/6), a indicação de Zanin foi aprovada pelo Senado Federal por 58 votos a 18. Mais cedo, ele passou por sabatina na Comissão de Constituição e Justiça da casa, onde teve o nome aprovado por 21 votos a 5."
         numComentarios={5}
         tags={["Zanin", "Senado", "ministro", "supremo", "stf"]}
       />
-
       <Desestruturado
         titulo="Comerciante que pode ser 4º filho de Gugu"
         conteudo="Filhos do apresentador foram notificados em audiência nesta quarta-feira (21). Advogado que defende Rose Miriam, mãe dos três filhos de Gugu, disse que, se confirmada a paternidade, o testamento feito pelo apresentador em 2011 é invalidado. Gugu morreu em 2019."
@@ -66,9 +69,13 @@ function App() {
       />
       <br />
       <br />
-
       {/* 06 - UseState(Hook) --------------- */}
       <State />
+      <br />
+      <br />
+      {/* 08 - Type ------------------------- */}
+      {umTexto && <div>"tem texto na variavel unTexto"</div>}
+      {outroTexto && <div>"tem texto na variavel outroTexto"</div>}
       <br />
       <br />
     </div>
