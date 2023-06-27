1a331a57ff8a352add92ae88262d872c9271d36c
denieldiniz <denieldiniz@gmail.com>
2023-06-27 08:35:17 -0300

    cabecalho e rodape

---
+++ 12_todo_react/src/App.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import styles from "./App.module.css";
+function App() {
+  return (
+    <div>
+      <Header />
+      <main className={styles.main}>
+        <div>
+          <h2>O que você vai fazer?</h2>
+          <p>formulário</p>
+        </div>
+        <div>
+          <h2>Suas tarefas:</h2>
+          <p>lista</p>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
